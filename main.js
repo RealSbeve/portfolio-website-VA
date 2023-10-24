1ec737c118c3f2d29aba618fd8d507525e259bf7
RealSbeve <juanmuelandrada97@gmail.com>
2023-10-24 09:30:18 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// const button = document.querySelector('.button');
+// const navbar = document.querySelector('.navbar');
+
+// button.addEventListener('click', () => {
+//     const isOpened = button.getAttribute('aria-expanded') === 'true';
+
+//     if (isOpened) {
+//         button.setAttribute('aria-expanded', 'false');
+//         navbar.setAttribute('aria-expanded', 'false');
+//     } else {
+//         button.setAttribute('aria-expanded', 'true');
+//         navbar.setAttribute('aria-expanded', 'true');
+//     }
+// });
